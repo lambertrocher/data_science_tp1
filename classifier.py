bd84907494c136fa384a744f77963043ab410e33
lambertrocher <lambert.rocher@gmail.com>
2018-01-01 21:02:40 +0100

    adaboost on adaline (poor results)

--- classifier.py
+++ classifier.py
@@ -11,7 +11,7 @@ def read_data(file, prediction):
     for line in file_opened.readlines():
         line_strip = line.strip().split(',')
         if i == 0:
-            nb_attributes = len(line_strip) -1
+            nb_attributes = len(line_strip) - 1
         if len(line_strip) == nb_attributes + 1:
             array.append(line_strip)
         i += 1
@@ -30,33 +30,37 @@ def read_data(file, prediction):
 
 
 class Classifier(object):
-    def __init__(self, dataset=None):
-        self.dataset = dataset
-        if dataset is not None:
-            self.train_dataset, self.test_dataset = self.split_dataset(proportion=0.75)
-            self.nb_explanatory_var = len(self.train_dataset[0]) - 1
-            self.nb_sample = len(dataset)
-        self.w = [0.0] * self.nb_explanatory_var
-        self.w0 = 0
+    def __init__(self):
+        self.dataset = None
+        self.train_dataset = None
+        self.test_dataset = None
+        self.nb_explanatory_var = None
+        self.nb_sample = None
+        self.w = None
+        self.w0 = None
 
     def learn(self, max_iter, eta, dataset=None, visualization=False):
         if visualization:
             vis = Visualization(3, 9)
-        samples = []
-        if dataset is None:
-            dataset = self.dataset
-            nb_sample = self.nb_sample
         else:
-            nb_sample = len(dataset)
+            vis = None
+        samples = []
+        if dataset is not None:
+            self.dataset = dataset
+            self.train_dataset, self.test_dataset = self.split_dataset(proportion=0.75)
+            self.nb_explanatory_var = len(self.train_dataset[0]) - 1
+            self.nb_sample = len(dataset)
+            self.w = [0.0] * self.nb_explanatory_var
+            self.w0 = 0
         for _ in range(max_iter):
-            i = randint(0, nb_sample - 1)
+            i = randint(0, self.nb_sample - 1)
             x = dataset[i][1:]
             y = dataset[i][0]
             samples.append(list(dataset[i][1:]))
             self.learn_step(x, y, eta)
             if visualization:
                 vis.animate(self.w, self.w0, samples)
-        #print("w =", self.w, "w0 =", self.w0)
+        # print("w =", self.w, "w0 =", self.w0)
         return list(self.w), self.w0
 
     def learn_step(self, x, y, eta):
@@ -73,7 +77,7 @@ class Classifier(object):
             if (y * ((np.dot(self.w, x)) + self.w0)) > 0:
                 nb_correct = nb_correct + 1
         result = nb_correct / len(test_dataset)
-        #print("% of good classification =", result)
+        # print("% of good classification =", result)
         return result
 
     def split_dataset(self, proportion):
@@ -96,10 +100,23 @@ class Classifier(object):
             miss.append((y * ((np.dot(self.w, x)) + self.w0)) > 0)
         return miss, [x if x == 1 else -1 for x in miss]
 
+    def prediction(self, dataset=None):
+        if dataset is None:
+            dataset = self.train_dataset
+        prediction = []
+        for i in range(len(dataset)):
+            x = dataset[i][1:]
+            y = dataset[i][0]
+            if (y * ((np.dot(self.w, x)) + self.w0)) > 0:
+                prediction.append(1)
+            else:
+                prediction.append(-1)
+        return prediction
+
 
 class Perceptron(Classifier):
-    def __init__(self, dataset=None):
-        super().__init__(dataset)
+    def __init__(self):
+        super().__init__()
 
     def learn_step(self, x, y, eta=1):
         if y * ((np.dot(self.w, x)) + self.w0) <= 0:
@@ -108,8 +125,8 @@ class Perceptron(Classifier):
 
 
 class Adaline(Classifier):
-    def __init__(self, dataset=None):
-        super().__init__(dataset)
+    def __init__(self):
+        super().__init__()
 
     def learn_step(self, x, y, eta=1):
         prediction = ((np.dot(self.w, x)) + self.w0)
@@ -130,11 +147,8 @@ class Visualization(object):
             for j in range(len(self.graph_x)):
                 self.graph_y[j] = (-1 * w0 - 1 * w[0] * self.graph_x[j]) / w[1]
         self.plot.set_ydata(self.graph_y)
-        # self.plot_samples.set_xdata(np.array(samples)[:, 0])
-        # self.plot_samples.set_ydata(np.array(samples)[:, 1])
         self.plot_samples = plt.plot(np.array(samples)[:, 0], np.array(samples)[:, 1], 'o')
         plt.pause(0.02)
-
 
     @staticmethod
     def show():
@@ -159,7 +173,7 @@ class CrossValidation(object):
         for eta in self.eta_range:
             epsilon_eta_array = []
             for i in range(self.k):
-                adaline = Adaline(self.dataset)
+                adaline = Adaline()
                 vk, ek = self.split_validate_train(self.k, i)
                 adaline.learn(max_iter=self.max_iter, eta=eta, dataset=ek)
                 epsilon_eta_array.append(adaline.evaluate(test_dataset=vk))
@@ -180,33 +194,57 @@ class CrossValidation(object):
 
 
 class AdaBoost(Classifier):
-    def __init__(self, dataset):
-        super().__init__(dataset)
+    def __init__(self):
+        super().__init__()
 
     def learn(self, max_iter, eta, dataset=None, visualization=False):
+        if dataset is not None:
+            self.dataset = dataset
+            self.train_dataset, self.test_dataset = self.split_dataset(proportion=0.75)
+            self.nb_explanatory_var = len(self.train_dataset[0]) - 1
+            self.nb_sample = len(dataset)
+            self.w = [0.0] * self.nb_explanatory_var
+            self.w0 = 0
+        weight = [1 / len(self.train_dataset)] * len(self.train_dataset)
+        prediction_train = [0] * len(self.train_dataset)
+        prediction_test = [0] * len(self.test_dataset)
         for m in range(max_iter):
             adaline = Adaline()
-            w, w0 = adaline.learn(max_iter=2000, eta=eta, dataset=self.train_dataset)
-            #print(adaline.evaluate())
+            adaline.learn(max_iter=2000, eta=eta, dataset=self.train_dataset)
             miss, miss2 = adaline.miss(dataset=self.train_dataset)
-            err_m = np.dot(weights, miss) / sum(w)
+            err_m = np.dot(weight, miss) / sum(weight)
+            alpha_m = 0.5 * np.log((1 - err_m) / err_m)
+            weight = np.multiply(weight, np.exp(np.multiply(alpha_m, miss2)))
+            #prediction_train += np.multiply(alpha_m, adaline.prediction(dataset=self.train_dataset))
+            prediction_test += np.multiply(alpha_m, adaline.prediction(dataset=self.test_dataset))
+        prediction_test = np.sign(prediction_test)
+        y = np.array(self.test_dataset)[:, 0]
+        return prediction_test, y
+
+
+def main():
+    dataset = read_data(file="iris.data", prediction="Iris-setosa")
+    # print(dataset)
+    dataset = list(np.array(dataset)[:, 0:3])
+
+    adaline = Adaline()
+    adaline.learn(dataset=dataset, max_iter=500, eta=0.01, visualization=True)
+    print(adaline.evaluate())
+
+    #
+    # eta_range = [0.00001, 0.0001, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05]
+    # cross_validation = CrossValidation(dataset=dataset, k=5, eta_range=eta_range, max_iter=500)
+    # for _ in range(10):
+    #     cross_validation.cross_validation()
+
+    # adaboost = AdaBoost()
+    # prediction, y = adaboost.learn(dataset=dataset, max_iter=100, eta=0.005)
+    # perf = 0
+    # for i in range(len(prediction)):
+    #     if prediction[i] == y[i]:
+    #         perf += 1
+    # print(perf/len(prediction))
 
 
 if __name__ == '__main__':
-    dataset = read_data(file="ionosphere.data", prediction="b")
-    #print(dataset)
-    #dataset = list(np.array(dataset)[:, 0:3])
-
-    # perceptron = Perceptron(dataset)
-    # perceptron.learn(max_iter=2000, eta=0.1)
-    # perceptron.evaluate()
-    # adaline = Adaline(dataset)
-    # adaline.learn(max_iter=3000, eta=0.002, visualization=False)
-    # adaline.evaluate()
-    #
-    # eta_range = [0.00001, 0.0001, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05]
-    # cross_validation = CrossValidation(dataset=dataset, k=5, eta_range=eta_range, max_iter=3000)
-    # cross_validation.cross_validation()
-
-    adaboost = AdaBoost(dataset)
-    adaboost.learn(max_iter=10, eta=0.001)
+    main()
